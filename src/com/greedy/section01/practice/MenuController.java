4fa4d7728a1bb04bb2b060d8ac5cf5580e75fb8f
kimtaehun521 <rth0204@gmail.com>
2023-04-23 16:28:35 +0900

    menu 작업완료

--- src/com/greedy/section01/practice/MenuController.java
+++ src/com/greedy/section01/practice/MenuController.java
@@ -1,9 +1,9 @@
 package com.greedy.section01.practice;
 
-public class Application {
+public class MenuController {
 
 	public static void main(String[] args) {
-		
+		// TODO Auto-generated method stub
 
 	}
 
